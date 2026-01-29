4cea620c744e9d82ab6ec5345918818c3d397909
Hosam Zewain <hosam.zewain@gmail.com>
2026-01-29 16:50:03 +0200

    frontdeploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3001/api';
+const API_BASE = 'https://system.ihome-store.com:3001/api';
 
 // Generic fetch helper
 async function fetchApi<T>(endpoint: string, options?: RequestInit): Promise<T> {
